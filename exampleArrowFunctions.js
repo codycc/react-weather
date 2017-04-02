38a39530f80ec1523e530c1fef2237f524b5a112
Cody Condon <codycondon50@gmail.com>
2017-04-02 13:50:21 +1000

    add project to github

---
+++ exampleArrowFunctions.js
@@ -0,0 +1,18 @@
+function add (a, b) {
+  return a + b;
+}
+
+var add2 = (a, b) => {
+  return a + b;
+}
+
+var add3 = (a,b) => a + b;
+
+console.log(add3(5,6));
+
+console.log(add2(4,6));
+
+
+
+console.log(add(1,3));
+console.log(add(3,9));
